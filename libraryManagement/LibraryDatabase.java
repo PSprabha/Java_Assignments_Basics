80403702d2a9fb2f60ce66ad7a882263218e38d5
Sorna Prabhakaran P <154865676+PSprabha@users.noreply.github.com>
2025-04-06 10:33:29 +0530

    Add files via upload

---
+++ libraryManagement/LibraryDatabase.java
@@ -0,0 +1,24 @@
+package libraryManagement;
+
+public class LibraryDatabase {
+	 public void add(Book book)
+	 {
+		 System.out.print("add");
+	 }
+   public void delete(Book book)
+   {
+   	System.out.print("delete");
+   }
+   public void update(Book oldBook, Book newBook)
+   {
+   	System.out.print("update");
+   }
+   public void display()
+   {
+   	System.out.print("display");
+   }
+   public void search(String title)
+   {
+   	System.out.print("search");
+   }
+}
